6bb881db3b2ff451eaef0adc667d572c1c3a4b17
giurra8 <anadjurovich@yahoo.com>
2020-01-29 19:39:23 +0100

    Initial commit

---
+++ src/main/java/com/olmero/tenders/TendersApplication.java
@@ -0,0 +1,13 @@
+package com.olmero.tenders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TendersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TendersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olmero/tenders/TendersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olmero.tenders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TendersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
